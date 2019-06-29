5abc49bfdfd9a34573514329bf41d20a56545979
Dylan Voigt <dtlvoigt@gmail.com>
2019-06-29 12:33:55 -0700

    Began source file for asciilines program

---
+++ asciilines.cpp
@@ -0,0 +1,15 @@
+/*****************************
+ **                         **
+ **   Asciilines            **
+ **   Author: Dylan Voigt   **
+ **   HW1 for cs461P        **
+ **                         **
+ *****************************/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << argv[1] <<  "  " << argc << '\n';
+	return 0;
+}
