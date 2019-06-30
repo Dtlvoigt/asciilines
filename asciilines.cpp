b85d8528b30ac6297aeaac2d5a435e60155c6546
Dylan Voigt <dtlvoigt@gmail.com>
2019-06-29 17:06:01 -0700

    Cleaned up format and added additional comments

--- asciilines.cpp
+++ asciilines.cpp
@@ -1,8 +1,8 @@
 /*****************************
  **                         **
- **   Asciilines            **
+ **   Asciilines.cpp        **
  **   Author: Dylan Voigt   **
- **   HW1 for cs461P        **
+ **   CS461P HW1            **
  **                         **
  *****************************/
 
@@ -13,34 +13,12 @@ char **table;
 int numRows;
 int numCols;
 
-/*void loadFile(std::string filename) ////////////////////////
-{
-	//std::cout << filename + '\n';
-	//std::ifstream* testFile = new std::ifstream(filename);
-
-	std::fstream testFile;
-	testFile.open(filename);
-
-	if(!testFile.is_open())
-	{
-		std::cerr << "\nCannot open " + filename + "\n\n";
-		exit(-2);
-	}
-
-}*/ /////////////////
-
+//reads canvas size from file and initializes 2d array
 void initializeTable(std::ifstream &testFile)
 {
-	//int xSize, ySize;
-	//testFile >> xSize;
 	testFile >> numRows;
-	//testFile >> ySize;
 	testFile >> numCols;
 	
-	std::cout << '\n' << numRows << " " << numCols << '\n'; ///////
-	//std::cout << '\n' << xSize << " " << ySize << '\n';
-
-	//table = new *int[numRows][numCols];
 	table = new char*[numRows];
 	for(int i = 0; i < numRows; i++)
 		table[i] = new char[numCols];
@@ -52,13 +30,14 @@ void initializeTable(std::ifstream &testFile)
 	return;
 }
 
+//parses and executes individual commands from file
 void parseCommand(std::ifstream &testFile)
 {
 	char render, orientation;
 	int startRow, startCol, length;
 	
+	//read info into variables
 	testFile >> render >> startRow >> startCol >> orientation >> length;
-	std::cout << render << startRow << startCol << orientation << length << '\n'; //////////
 
 	int xLength = 1;
 	int yLength = 1;
@@ -68,6 +47,7 @@ void parseCommand(std::ifstream &testFile)
 	if(orientation == 'v')
 		yLength = length;
 
+	//add render character to table if it falls within the canvas size
 	for(int i = startRow; i < (startRow + yLength); i++)
 	{
 		for(int j = startCol; j < (startCol + xLength); j++)
@@ -78,48 +58,49 @@ void parseCommand(std::ifstream &testFile)
 	}
 }
 
+//prints contents of 2d array to screen
 void printTable()
 {
 	for(int i = 0; i < numRows; i++) 
 	{
 		for(int j = 0; j < numCols; j++)
-			std::cout << table[i][j] << ' ';
+			std::cout << table[i][j];
+			//std::cout << table[i][j] << ' ';
 		std::cout << '\n';
 	}
 }
 
 int main(int argc, char* argv[])
 {
+	//exit if input is invalid
 	if(argc != 2) 
 	{
-		std::cerr << "Incorrect number of args.\n";
+		std::cerr << "EXIT: Incorrect number of args.\n";
 		return -1;
 	}
 
+	//attempt to open specified file
 	std::ifstream testFile;
-
 	testFile.open(argv[1]);
+
+	//exit if file cannot be opened
 	if(!testFile.is_open())
 	{
-		std::cerr << "\nCannot open " << argv[1] << "\n\n";
+		std::cerr << "EXIT: Cannot open " << argv[1] << '\n';
 		return -2;
 	}
 
+	//set array bounds and initialize with '.'s
 	initializeTable(testFile);
 
-	std::string temp;
+	//run commands until eof is reached
 	while(!testFile.eof())
 	{
 		parseCommand(testFile);
-		testFile >> std::ws;
-		//int c = testFile.peek();
-		//if(c == EOF)
-			//return 0;
-		//getline(testFile, temp);
+		testFile >> std::ws; //eliminate whitespace
 	}
 
-	//loadFile(argv[1]);
-
+	//print contents of array
 	printTable();
 
 	testFile.close();
